fa824ae8e4e4f89bc2213fa4bfb1602c887cd291
Daniel-Penaloza <danielpenalozatj@gmail.com>
2020-06-16 20:03:23 -0700

    Mapping On Ruby

---
+++ map.rb
@@ -0,0 +1,17 @@
+class Array
+    def my_map
+      c = 0
+      acc = []
+      until c == size
+        acc << "#{yield self[c]}"
+        c += 1
+      end
+      acc
+    end
+  end
+  
+  
+
+names = ["David", "Alan", "Black"]
+puts names.my_map {|name| name.upcase}
+
