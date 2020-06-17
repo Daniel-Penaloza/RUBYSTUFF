bdc4fa84202e8b01799b942f4dd1ac3a28ff82fe
Daniel-Penaloza <danielpenalozatj@gmail.com>
2020-06-16 21:07:48 -0700

    Counting Characters

--- codewars/count_characters.rb
+++ codewars/count_characters.rb
@@ -3,7 +3,22 @@
 #What if the string is empty? Then the result should be empty object literal, {}.
 
 def count_chars(s)
-    puts s.to_h
+    h = Hash.new
+    s.each_char.each do |c|
+        s.count(c) > 1 ?  h["#{c}"] = s.count(c) : h["#{c}"] = s.count(c)
+=begin
+        Is the same as the one line ternary operation
+        if s.count(c) > 1
+            h["#{c}"] = s.count(c)
+        else
+            h["#{c}"] = s.count(c)
+        end
+=end
+    end
+    h
 end
 
 count_chars("aba")
+count_chars("ciclopentanoperhidrofenantreno")
+count_chars("")
+
